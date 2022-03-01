0e32eb085dbd34c02a185efacccdcec7b7292a10
snegapriya <snegapriya20@gmail.com>
2022-03-01 13:30:49 +0530

    java

---
+++ src/com/exception/ExceptionExample.java
@@ -0,0 +1,18 @@
+package com.exception;
+
+import java.util.Scanner;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		
+        //StringIndexOutOfBoundsException
+		
+		String s = "SheelaRani";
+		
+		char charAt = s.charAt(11);
+
+		System.out.println(charAt);
+		
+	}
+}
